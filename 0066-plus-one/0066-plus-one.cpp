3af9d9a36720983f9cc043539d7656fcb329f4e9
rajskider <rajskider1588@gmail.com>
2026-08-07 16:14:06 +0530

    Time: 0 ms (100%), Space: 11.7 MB (28.03%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int i = digits.size() - 1;
+        if(digits[i] != 9) digits[i]++;
+        else{
+            while(i >= 0 && digits[i] == 9){
+                digits[i] = 0;
+                i--;
+            }
+            if(i >= 0) digits[i]++;
+            else digits.insert(digits.begin(), 1);
+        }
+        return digits;
+    }
+};
