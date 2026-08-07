b530bc278164f433ec0d66bcd2beb62c2c1d3432
rajskider <rajskider1588@gmail.com>
2026-08-07 23:29:45 +0530

    Time: 0 ms (100%), Space: 9.4 MB (41.68%) - LeetHub

---
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        int pos;
+        while((pos = s.find(part)) != string::npos)
+            s.erase(pos, part.size());
+        return s;
+    }
+};
