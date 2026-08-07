cb8dcf630777e9afa346ccee92fbde9797cc0b60
rajskider <rajskider1588@gmail.com>
2026-08-07 20:27:16 +0530

    Time: 0 ms (100%), Space: 10.1 MB (30.58%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string ans;
+        for(char c : s)
+            if(isalnum(c)) ans.push_back(tolower(c));
+        for(int i = 0; i < ans.size() / 2; i++)
+            if(ans[i] != ans[ans.size() - i - 1]) return false;
+        return true;
+    }
+};
