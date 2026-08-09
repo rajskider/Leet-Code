50b7b4fd9ee5222b1f92b2215ea8b3f0b42b8db0
rajskider <rajskider1588@gmail.com>
2026-08-09 18:03:29 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.96%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int start = 0, end = x;
+        while(start <= end) {
+            long long mid = start + (end - start) / 2;
+            if(mid * mid == x) return mid;
+            else if(mid * mid < x) start = mid + 1;
+            else end = mid - 1;
+        }
+        return end;
+    }
+};
